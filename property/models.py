718b2e334ec2f910164ca80a2302885744606424
veffhz <veffhz@example.com>
2020-11-18 19:00:25 +0300

    Add likes fields

--- property/models.py
+++ property/models.py
@@ -24,6 +24,8 @@ class Flat(models.Model):
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
     new_building = models.NullBooleanField("Признак новостройки")
 
+    likes = models.ManyToManyField(User, verbose_name='Кто лайкнул')
+
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
 
