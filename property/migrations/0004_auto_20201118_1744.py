ac84532b84d24b87017818b9e271e1f0706d2872
veffhz <veffhz@example.com>
2020-11-19 19:28:07 +0300

    Fixed redundant condition

--- property/migrations/0004_auto_20201118_1744.py
+++ property/migrations/0004_auto_20201118_1744.py
@@ -6,7 +6,7 @@ from django.db import migrations
 def set_new_building_field(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat') # noqa
     for flat in Flat.objects.all():
-        flat.new_building = True if flat.construction_year >= 2015 else False
+        flat.new_building = flat.construction_year >= 2015
         flat.save()
 
 
